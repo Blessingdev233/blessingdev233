1c1dce7d9ee15f396af6bd7c3e1e2f6f972c4b87
Blessingdev233 <blessing.lartey@gmail.com>
2022-09-02 17:04:06 +0000

    scripts

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
-// index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+import * as dotenv from 'dotenv'
+dotenv.config()
+import Mustache from 'mustache'
+import { readFileSync, writeFileSync } from 'node:fs'
+import got from 'got'
 
-let DATA = {
-  name: 'Blessing Tyron',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
-    timeZone: 'GMT',
-  }),
-};/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
-    if (err) throw err;
-    const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
-  });
-}generateReadMe();
+const MUSTACHE_MAIN_DIR = './main.mustache'
+
+const lastRefresh = new Date().toLocaleDateString('en-GB', {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  timeZoneName: 'short',
+  timeZone: 'GMT',
+})
+
+const techs = [
+  {
+    name: 'Nuxt.js',
+    website: 'https://nuxtjs.org',
+    logo: 'nuxt.js',
+    badgeColor: '00C58E',
+  },
+  {
+    name: 'Tailwind%20CSS',
+    website: 'https://tailwindcss.com',
+    logo: 'tailwind-css',
+    badgeColor: '38B2AC',
+  },
+  {
+    name: 'Sanity',
+    website: 'https://www.sanity.io',
+    logo: '',
+    badgeColor: 'fa1607',
+  },
+  {
+    name: 'Cypress',
+    website: 'https://www.cypress.io',
+    logo: 'cypress',
+    badgeColor: '17202C',
+  },
+  {
+    name: 'Netlify',
+    website: 'https://www.netlify.com',
+    logo: 'netlify',
+    badgeColor: '00C7B7',
+  },
+]
+
+async function fetchArticles() {
+  return got('https://dev.to/api/articles?username=mornir&per_page=5').json()
+}
+
+
+
+
